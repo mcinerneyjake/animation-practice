ca8361949e932cf0c8a0b7da6838a091e6b736af
Jake McInerney <jakemcinerney1@gmail.com>
2022-02-26 11:29:37 -0600

    added new JS for top-left

--- animation.js
+++ animation.js
@@ -1,9 +1,39 @@
+// This is the Center text.
 const textCenter = document.querySelector(".center");
-const stringTextCenter = text.textContent;
+const stringTextCenter = textCenter.textContent;
 const splitTextCenter = stringTextCenter.split("");
+textCenter.textContent = "";
 
 for (let i = 0; i < splitTextCenter.length; i++) {
-  text.innerHTML += "<span>" + splitTextCenter[i] + "</span>";
+  textCenter.innerHTML += "<span>" + splitTextCenter[i] + "</span>";
+}
+
+let character = 0;
+let timer = setInterval(onRefresh, 50);
+
+function onRefresh() {
+  const span = text.querySelectorAll("span")[character];
+  span.classList.add("center-each");
+  character++;
+  if (character === splitTextCenter.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
+
+// This is the Top Left text.
+const textCenter = document.querySelector(".center");
+const stringTextCenter = textCenter.textContent;
+const splitTextCenter = stringTextCenter.split("");
+textCenter.textContent = "";
+
+for (let i = 0; i < splitTextCenter.length; i++) {
+  textCenter.innerHTML += "<span>" + splitTextCenter[i] + "</span>";
 }
 
 let character = 0;
