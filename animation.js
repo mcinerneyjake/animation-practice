4c2502f1bc48e3f07dc17a93b93c61b840e0a8e9
Jake McInerney <jakemcinerney1@gmail.com>
2022-02-26 12:01:09 -0600

    added simple animation to center p tag

--- animation.js
+++ animation.js
@@ -12,7 +12,7 @@ let character = 0;
 let timer = setInterval(onRefresh, 50);
 
 function onRefresh() {
-  const span = text.querySelectorAll("span")[character];
+  const span = textCenter.querySelectorAll("span")[character];
   span.classList.add("center-each");
   character++;
   if (character === splitTextCenter.length) {
@@ -27,29 +27,29 @@ function complete() {
 }
 
 // This is the Top Left text.
-const textCenter = document.querySelector(".center");
-const stringTextCenter = textCenter.textContent;
-const splitTextCenter = stringTextCenter.split("");
-textCenter.textContent = "";
+/*const textTopLeft = document.querySelector(".top-left");
+const stringTextTopLeft = textTopLeft.textContent;
+const splitTextTopLeft = stringTextTopLeft.split("");
+textTopLeft.textContent = "";
 
-for (let i = 0; i < splitTextCenter.length; i++) {
-  textCenter.innerHTML += "<span>" + splitTextCenter[i] + "</span>";
+for (let i = 0; i < splitTextTopLeft.length; i++) {
+  textTopLeft.innerHTML += "<span>" + splitTextTopLeft[i] + "</span>";
 }
 
-let character = 0;
-let timer = setInterval(onRefresh, 50);
+let character1 = 0;
+let timer1 = setInterval(onRefresh1, 50);
 
-function onRefresh() {
-  const span = text.querySelectorAll("span")[character];
-  span.classList.add("center-each");
-  character++;
-  if (character === splitTextCenter.length) {
-    complete();
+function onRefresh1() {
+  const span = text.querySelectorAll("span")[character1];
+  span.classList.add("top-left-each");
+  character1++;
+  if (character1 === splitTextTopLeft.length) {
+    complete1();
     return;
   }
 }
 
-function complete() {
-  clearInterval(timer);
-  timer = null;
-}
+function complete1() {
+  clearInterval(timer1);
+  timer1 = null;
+} */
